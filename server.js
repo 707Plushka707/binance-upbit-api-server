de1c2a723483d7fd585fb913874ceaa14bb718c1
HWRdeveloper <54504396+HWRdeveloper@users.noreply.github.com>
2021-06-01 16:53:47 +0900

    premium calculated locally and distributed through api server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(helmet());
 
-const server = app.listen(8080, () =>{
+const server = app.listen(3000, () =>{
     console.log()
 });
 
@@ -20,18 +20,30 @@ app.get('/api/binance/price', async (req, res) => {
 });
 
 app.get('/api/upbit/price', async (req, res) => {
-    res.send(sJson);
+    res.send(uJson);
+
+});
+
+app.get('/api/premium', async (req, res) => {
+    
+    res.send(premiumJson);
 
 });
 
 var price_list = ''
 var sJson = '';
+var aArray = new Array();
 var uJson = '';
+var premiumJson = '';
 const Binance = require('node-binance-api');
 const binance = new Binance().options({
   APIKEY: 'AsuW8pjiAx6XVWsKGxTnscWS6xfaeJa97KOSGY3CHbw8MTml5E2V0jk5EhpoBLC3',
   APISECRET: 'k9xip6mFjadZWHcrpyqCe7vYW9HTCCtbUSCTE7WZZJSo43KqFiAD5El26y6ERSqK'
 });
+
+//환율  변경 요기서
+
+var rate = 1107;
 
 
 const fetch = require('node-fetch');
@@ -44,62 +56,7 @@ const timer=ms => new Promise(res=>setTimeout(res, ms))
 
 async function load() {
     while(true){
-        binance.prices(['BNBBTC' ,'ETHBTC'], (error, ticker) => {
-            
-            
-
-            var aJsonArray = new Array();
-
-            var aJson = new Object(); 
-            aJson.symbol = "BTCUSDT";
-            aJson.price = ticker.BTCUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "ETHUSDT";
-            aJson.price = ticker.ETHUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "ADAUSDT";
-            aJson.price = ticker.ADAUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "XRPUSDT";
-            aJson.price = ticker.XRPUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "DOGEUSDT";
-            aJson.price = ticker.DOGEUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "DOTUSDT";
-            aJson.price = ticker.DOTUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "LTCUSDT";
-            aJson.price = ticker.LTCUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "VETUSDT";
-            aJson.price = ticker.VETUSDT;
-            aJsonArray.push(aJson);
-
-            var aJson = new Object(); 
-            aJson.symbol = "EOSUSDT";
-            aJson.price = ticker.EOSUSDT;
-            aJsonArray.push(aJson);
-            
-            sJson = JSON.stringify(aJsonArray);
-
-            console.info(sJson);
-
-          });
+        
 
 
           const url = 'https://api.upbit.com/v1/ticker?markets=KRW-BTC, KRW-ETH, KRW-ADA, KRW-XRP, KRW-DOGE, KRW-DOT, KRW-LTC, KRW-VET, KRW-EOS';
@@ -108,19 +65,96 @@ async function load() {
           await fetch(url, options)
             .then(res => res.json())
             .then(json => {
-                bJson = JSON.stringify(json)})
+                bJson = json})
             .catch(err => console.error('error:' + err));
             
             var uJsonArray = new Array();
             for( var i = 0; i< bJson.length; i++ ) {
-
-
+                var aJson = new Object(); 
+                aJson.symbol = bJson[i]['market'];
+                aJson.price = bJson[i]['trade_price'];
+                uJsonArray.push(aJson);
             }
-            aJson.symbol = bJson[i].market;
-            aJson.price = ticker.BTCUSDT;
-            uJsonArray.push(aJson);
+
+            binance.prices(['BNBBTC' ,'ETHBTC'], (error, ticker) => {
+                var aJsonArray = new Array();
+    
+                var aJson = new Object(); 
+                aJson.symbol = "BTCUSDT";
+                aJson.price = ticker.BTCUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "ETHUSDT";
+                aJson.price = ticker.ETHUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "ADAUSDT";
+                aJson.price = ticker.ADAUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "XRPUSDT";
+                aJson.price = ticker.XRPUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "DOGEUSDT";
+                aJson.price = ticker.DOGEUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "DOTUSDT";
+                aJson.price = ticker.DOTUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "LTCUSDT";
+                aJson.price = ticker.LTCUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "VETUSDT";
+                aJson.price = ticker.VETUSDT;
+                aJsonArray.push(aJson);
+    
+                var aJson = new Object(); 
+                aJson.symbol = "EOSUSDT";
+                aJson.price = ticker.EOSUSDT;
+                aJsonArray.push(aJson);
+                
+                sJson = JSON.stringify(aJsonArray);
+                aArray = aJsonArray;
+    
+                //console.info(sJson);
+                var premiumArray = new Array();
+
+                for (var i = 0 ; i < uJsonArray.length ; i++){
+                    var aJson = new Object(); 
+                    aJson.symbol = uJsonArray[i]['symbol'];
+                    var number = (parseFloat(uJsonArray[i]['price']) / (parseFloat(aJsonArray[i]['price'])* rate)) * 100 - 100;
+                    
+                    aJson.premium = (Math.floor(number*100)/100).toFixed(2);
+                    premiumArray.push(aJson);
+                }
+
+                premiumJson = JSON.stringify(premiumArray);
+    
+              });
+
             
-          console.log(bJson);
+
+            
+
+            
+
+            
+            
+            uJson = JSON.stringify(uJsonArray);
+            console.log(premiumJson);
+
+
         await timer(500);
 
         
